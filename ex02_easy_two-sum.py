2d210fbb5fe0775b0cb60309fe425c1c22d337f1
andradebru <brunansandrade@gmail.com>
2023-01-11 20:28:10 -0300

    ex 02 solved

---
+++ ex02_easy_two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            second_number = target - nums[i]
+            if second_number in nums:
+                if i!=nums.index(second_number):
+                    return [i, nums.index(second_number)]
