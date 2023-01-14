7b1ecac16e1c3bf36157abce6e31bdb2d541c286
andradebru <brunansandrade@gmail.com>
2023-01-14 19:09:20 -0300

    ex01 format

--- easy/ex_01_easy_roman-to-integer.js
+++ easy/ex_01_easy_roman-to-integer.js
@@ -4,40 +4,37 @@
  * @param {string} string
  * @return {number}
  */
-var romanToInt = function(string) {
-    var numbersMap = new Map(
-        [
-            ["I", 1],
-            ["V", 5],
-            ["X", 10],
-            ["L", 50],
-            ["C", 100],
-            ["D", 500],
-            ["M", 1000],
-        ]
-    )
+let romanToInt = function (string) {
+  const numbersMap = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
 
-    i = string.length - 1
-    result = 0
+  let i = string.length - 1;
+  let result = 0;
 
-    for (i; i >= 0; i--) {
-        if (i == 0) {
-            result += numbersMap.get(string[i])
-        }
-        else if (numbersMap.get(string[i]) > numbersMap.get(string[i - 1])) {
-            result += numbersMap.get(string[i]) - numbersMap.get(string[i - 1])
-            i--
-        } else {
-            result += numbersMap.get(string[i])
-        }
+  for (i; i >= 0; i--) {
+    if (i == 0) {
+      result += numbersMap.get(string[i]);
+    } else if (numbersMap.get(string[i]) > numbersMap.get(string[i - 1])) {
+      result += numbersMap.get(string[i]) - numbersMap.get(string[i - 1]);
+      i--;
+    } else {
+      result += numbersMap.get(string[i]);
     }
-    
-    return result
+  }
+
+  return result;
 };
 
 // change string to test other scenarios
 
-console.log(romanToInt('XVII'))
-console.log(romanToInt('IX'))
-console.log(romanToInt('V'))
-console.log(romanToInt('DCIVXX'))
+console.log(romanToInt("XVII"));
+console.log(romanToInt("IX"));
+console.log(romanToInt("V"));
+console.log(romanToInt("DCIVXX"));
